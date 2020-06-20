f7e90c3fb4d66bff6c4b8271c02a88247dfa0068
Carsten Strotmann <carsten@strotmann.de>
2020-06-20 18:57:46 +0200

    Renamed Directory for Atari ST

--- tools/gensourcefiles.sh
+++ tools/gensourcefiles.sh
@@ -4,7 +4,31 @@ mkdir -p $d/../sources/msdos
 for i in $d/../msdos/*.{fb,vid,sys}; do
   b=$(basename $i)
   /bin/echo -n "write $d/../sources/msdos/$b.src ..."
-  $d/dumpblock.sh "$i" > $d/../sources/msdos/$b.src 
+  $d/dumpblock.sh "$i" > $d/../sources/msdos/$b.src
+  echo " Done."
+done
+
+mkdir -p $d/../sources/cpm
+for i in $d/../cpm/*.FB; do
+  b=$(basename $i)
+  /bin/echo -n "write $d/../sources/cpm/$b.src ..."
+  $d/dumpblock.sh "$i" > $d/../sources/cpm/$b.src
+  echo " Done."
+done
+
+mkdir -p $d/../sources/AtariST
+for i in $d/../AtariST/*.FB; do
+  b=$(basename $i)
+  /bin/echo -n "write $d/../sources/AtariST/$b.src ..."
+  $d/dumpblock.sh "$i" > $d/../sources/AtariST/$b.src
+  echo " Done."
+done
+
+mkdir -p $d/../sources/AtariST/GEM
+for i in $d/../AtariST/GEM/*.FB; do
+  b=$(basename $i)
+  /bin/echo -n "write $d/../sources/AtariST/GEM/$b.src ..."
+  $d/dumpblock.sh "$i" > $d/../sources/AtariST/GEM/$b.src
   echo " Done."
 done
 

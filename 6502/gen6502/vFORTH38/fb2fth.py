ae1f4d9dffe8c0a11d67c9bb2f17895cff43a3f2
Philip Zembrod <pzembrod@google.com>
2020-06-27 23:40:25 +0200

    Makefile and python3 script to turn .fb block sources into .fth text files

---
+++ 6502/gen6502/vFORTH38/fb2fth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+
+def readToString(inFile):
+  blockNo = 0
+  result = []
+  while(True):
+    block = inFile.read(1024)
+    if len(block) == 0:
+      break
+    result.append("\n\\ *** Block No. %d, Hexblock %x\n"
+        % (blockNo, blockNo));
+    offset = 0
+    while(offset < len(block)):
+      # sys.stderr.write("block %d offset %d\n" % (blockNo, offset))
+      line = block[offset:offset+64].decode(encoding="latin_1")
+      result.append(line.rstrip())
+      offset += 64
+    blockNo += 1
+  return result
+
+inFileName, outFileName = sys.argv[1], sys.argv[2]
+inFile = open(inFileName, "rb")
+result = readToString(inFile)
+result.append('')
+outFile = open(outFileName, "w")
+outFile.write("\n".join(result))

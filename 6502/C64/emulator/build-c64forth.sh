67e498ccef0a2c1aa0c6ac4d3ecd1edf04d8a6f9
Philip Zembrod <pzembrod@google.com>
2020-07-05 21:24:39 +0200

    Rename mk-c64forth.fth to vf-c64-main.fth. Add some comments

--- 6502/C64/emulator/build-c64forth.sh
+++ 6502/C64/emulator/build-c64forth.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Script for target-compiling the C64 version of VolksForth.
+# It uses the tcbase binary, i.e. the resident part of the target
+# compiler, adds the transient part of the target compiler, and then
+# loads the C64 VolksForth sources, and saves the target.
+
 set -e
 
 emulatordir="$(dirname "${BASH_SOURCE[0]}")"
@@ -6,7 +12,7 @@ basedir="$(realpath --relative-to="$PWD" "${emulatordir}/..")"
 
 rm -f "${basedir}/cbmfiles/c64-vf-latest"
 
-keybuf="include mk-c64forth.fth\n\
+keybuf="include vf-c64-main.fth\n\
 save-target c64-vf-latest\ndos s0:notdone\n"
 
 DISK9=vforth4_2 DISK10=tc38q "${emulatordir}/run-in-vice.sh" \

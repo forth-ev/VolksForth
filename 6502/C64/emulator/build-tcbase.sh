50d3ba749967e2955daf9e2b3ad934e68deba162
Philip Zembrod <pzembrod@google.com>
2020-07-05 21:13:04 +0200

    Rename mk-tcbase.fth to tc-base.fth, and add some comments.

--- 6502/C64/emulator/build-tcbase.sh
+++ 6502/C64/emulator/build-tcbase.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# build script for the resident part of the target compiler running
+# on the C64.
+
 set -e
 
 emulatordir="$(dirname "${BASH_SOURCE[0]}")"
@@ -6,8 +9,8 @@ basedir="$(realpath --relative-to="$PWD" "${emulatordir}/..")"
 
 rm -f "${basedir}/cbmfiles/tcbase"
 
-keybuf="2 drive 4 load\ninclude mk-tcbase.fth\n\
+keybuf="2 drive 4 load\ninclude tc-base.fth\n\
 savesystem tcbase\ndos s0:notdone\n"
 
-DISK9=vforth4_2 DISK10=tc38q "${emulatordir}/run-in-vice.sh" \
+DISK10=tc38q "${emulatordir}/run-in-vice.sh" \
   "c64-testbase" "${keybuf}"
